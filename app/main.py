72cc92421921d0be276462ca5770de7cc7af2109
aashitachavhan <chavhanaashita@gmail.com>
2026-02-23 23:59:15 +0530

    middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routes import upload, chat
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Logistics RAG System")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # For development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload.router)
 app.include_router(chat.router)
